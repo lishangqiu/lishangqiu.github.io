dbffbe99716a13d4e985b2e7a539b29dcf332d69
lishangqiu <lishangqiu0930@gmail.com>
2022-02-23 19:00:40 -0800

    fixed formatting problem

--- js/index_page/main_background.js
+++ js/index_page/main_background.js
@@ -2,17 +2,16 @@ var canvas = document.getElementsByTagName('canvas')[0];
 var ctx = canvas.getContext('2d');
 
 function assignToDiv(){ // this kind of function you are looking for
-  canvas.height = document.getElementById('main-body').offsetHeight;
-  canvas.width = document.getElementById('main-body').offsetWidth;
 }
 
+console.log(canvas.height)
+
 // bad code, but i don't know any other way.
-document.getElementById('main-body').setAttribute("style",`height:${$(document).height()-50}px`);
 assignToDiv();
 var nodesjs = new NodesJs({
     id: 'nodes',
-    width: canvas.width,
-    height: canvas.height,
+    width: window.innerWidth,
+    height: window.innerHeight,
 
     backgroundFrom: [0, 5, 60],
     backgroundTo: [10, 25, 75],
